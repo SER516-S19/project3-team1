d0ea26aa7f11a40c494e62b4f677761d66d1833f
John Alden <jzalden96@gmail.com>
2019-04-05 02:24:53 -0700

    Add Quiz and Question Daos

--- src/com/team1/project3/quizdao/Quiz.java
+++ src/com/team1/project3/quizdao/Quiz.java
@@ -1,4 +1,38 @@
 package com.team1.project3.quizdao;
 
-public class Quiz {
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Quiz implements Iterable<Question> {
+    private List<Question> questionList;
+    private String title;
+
+    public Quiz(String name) {
+        questionList = new LinkedList<Question>();
+        this.title = name;
+    }
+
+    void addQuestion(Question new_question) {
+        questionList.add(new_question);
+    }
+
+    Question getQuestion(int index) {
+        return questionList.get(index);
+    }
+
+    int getQuestionCount() {
+        return questionList.size();
+    }
+
+    @Override
+    public Iterator<Question> iterator() {
+        return questionList.iterator();
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
+
+
