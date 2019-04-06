ed7ebca2cb2a5f0669685d0a4cdbd614fb99d411
Pradeep Ambalam Jawaharlal <pambalam@asu.edu>
2019-04-05 20:15:15 -0700

    Issue #17 Refactored json parsing and file read mechanism

--- professor/src/main/java/ProfessorViewController.java
+++ professor/src/main/java/ProfessorViewController.java
@@ -1,2 +1,22 @@
+import javax.swing.*;
+
 public class ProfessorViewController {
+    private ProfessorView professorView;
+    private ProfessorModel professorModel;
+    public ProfessorViewController(ProfessorView professorView, ProfessorModel professorModel) {
+        this.professorView = professorView;
+        this.professorModel = professorModel;
+        initializeView();
+    }
+
+    private void initializeView() {
+        JFrame frame = new JFrame("Professor");
+        frame.setContentPane(professorView.$$$getRootComponent$$$());
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+
 }
--- quizcore/src/dao/Quiz.java
+++ quizcore/src/dao/Quiz.java
@@ -1,37 +1,14 @@
 package dao;
 
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
-public class Quiz implements Iterable<Question> {
-    private List<Question> questionList;
-    private String title;
+public class Quiz
+{
+    public List<Question> questions;
 
-    public Quiz(String name) {
-        questionList = new LinkedList<Question>();
-        this.title = name;
-    }
-
-    public void addQuestion(Question new_question) {
-        questionList.add(new_question);
-    }
-
-    public Question getQuestion(int index) {
-        return questionList.get(index);
-    }
-
-    public int getQuestionCount() {
-        return questionList.size();
-    }
-
-    @Override
-    public Iterator<Question> iterator() {
-        return questionList.iterator();
-    }
-
-    public String getTitle() {
-        return title;
+    public Quiz() {
+        questions = new ArrayList<Question>();
     }
 }
 
