7e1683af6d830a60ea197188008e76da1c365293
MayankBatra005 <er.mayank005@gmail.com>
2019-04-06 13:05:53 -0700

    U8T10 : Load question in student app

--- student/src/main/java/quiz/StudentViewController.java
+++ student/src/main/java/quiz/StudentViewController.java
@@ -1,4 +1,35 @@
 package main.java.quiz;
 
+import javax.swing.*;
+
 public class StudentViewController {
+    private StudentView studentView;
+    private StudentModel studentModel;
+    private int currentQuestionIndex = 0;
+    public StudentViewController(StudentView studentView, StudentModel studentModel) {
+        this.studentView = studentView;
+        this.studentModel = studentModel;
+        initializeView();
+        loadQuestion(currentQuestionIndex);
+    }
+
+    private void loadQuestion(int currentQuestionIndex) {
+        studentView.getQuizTitleLabel().setText(studentModel.getQuiz().questions.get(currentQuestionIndex).getTitle());
+    }
+
+    private void initializeView() {
+        JFrame frame = new JFrame("Student");
+        frame.setContentPane(studentView.$$$getRootComponent$$$());
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+
+
+
+
 }
+
+
