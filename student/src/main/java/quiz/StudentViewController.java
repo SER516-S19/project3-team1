676a5dfbd0241d9e75179523d5732fadc6df94df
krishnachandu <kakula2@asu.edu>
2019-04-06 21:13:02 -0700

    adding events on next buttons

--- student/src/main/java/quiz/StudentViewController.java
+++ student/src/main/java/quiz/StudentViewController.java
@@ -1,6 +1,8 @@
 package main.java.quiz;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class StudentViewController {
     private StudentView studentView;
@@ -10,10 +12,31 @@ public class StudentViewController {
         this.studentView = studentView;
         this.studentModel = studentModel;
         initializeView();
+        renderQuestionToFrame();
+        registerActionListenersForUIComponents();
+    }
+
+    private void renderQuestionToFrame(){
+
         loadQuestion(currentQuestionIndex);
         loadQuizTitle(currentQuestionIndex);
         loadOptions(currentQuestionIndex);
         loadQuestionNumber(currentQuestionIndex);
+        resetSelectionsOnRadioButtons();
+    }
+
+    private void registerActionListenersForUIComponents(){
+
+        registerActionListenerForNextButton();
+    }
+
+    private void resetSelectionsOnRadioButtons(){
+
+        studentView.getaRadioButton().setSelected(false);
+        studentView.getbRadioButton().setSelected(false);
+        studentView.getcRadioButton().setSelected(false);
+        studentView.getdRadioButton().setSelected(false);
+
     }
 
     private void loadOptions(int currentQuestionIndex) {
@@ -38,6 +61,7 @@ public class StudentViewController {
         studentView.getQuizTitleLabel().setText("Quiz "+quiz);
     }
 
+
     private void initializeView() {
         JFrame frame = new JFrame("Student");
         frame.setContentPane(studentView.$$$getRootComponent$$$());
@@ -47,7 +71,39 @@ public class StudentViewController {
         frame.setVisible(true);
     }
 
+    private void registerActionListenerForNextButton(){
 
+        studentView.getNextButton().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                evaluateAnswerAndLoadNextQuestion();
+            }
+        });
+    }
+    private void evaluateAnswerAndLoadNextQuestion(){
+
+        int selectedOptionIndex = 0;
+        if(studentView.getaRadioButton().isSelected()){
+            selectedOptionIndex=0;
+        }
+        else if(studentView.getbRadioButton().isSelected()){
+            selectedOptionIndex=1;
+        }
+        else if(studentView.getcRadioButton().isSelected()){
+            selectedOptionIndex=2;
+        }
+        else{
+            selectedOptionIndex=3;
+        }
+        studentModel.evaluateAnswer(currentQuestionIndex,selectedOptionIndex);
+
+        if (currentQuestionIndex < studentModel.getQuiz().questions.size()-1)
+            currentQuestionIndex+=1;
+        else
+            currentQuestionIndex=0;
+
+        renderQuestionToFrame();
+    }
 
 
 
